fc2ca034b7da7cc4a2fac4946f6f53f6a53fc370
Makhmudkhon Muniskhonov <little_7o7@Makhmudkhons-MacBook-Pro.local>
2022-08-14 00:00:18 +0500

    create links base

--- Learning/React/apple-website/src/components/Footer/Footer.js
+++ Learning/React/apple-website/src/components/Footer/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import Links from './Links';
+
 import './Footer.scss';
+
+console.log(<Links/>);
 
 const Footer = () => {
     return (
         <div className='Footer'>
-            
+            <div className="center">
+                <div className="top"></div>
+                <div className="links"></div>
+                <div className="bottom"></div>
+            </div>
         </div>
     );
 }
