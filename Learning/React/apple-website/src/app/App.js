9d591c80cf7b3f21a4248d7d70913e24122fb820
Makhmudkhon Muniskhonov <little_7o7@Makhmudkhons-MacBook-Pro.local>
2022-08-13 23:37:31 +0500

    footer started

--- Learning/React/apple-website/src/app/App.js
+++ Learning/React/apple-website/src/app/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
+
+import Navbar from '../components/Navbar/Navbar';
+import Unicef from '../components/Unicef/Unicef';
 
 import './App.scss';
 
 const App = () => {
     return (
         <div className='App'>
-            
+            <Navbar />
+            <Unicef />
+
+            <Footer />
         </div>
     );
 }
