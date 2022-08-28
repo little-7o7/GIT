4fd5c0ef8406dfff6ebc946b99f3e053fd5bb032
Makhmudkhon Muniskhonov <little_7o7@Makhmudkhons-MacBook-Pro.local>
2022-08-28 15:38:15 +0500

    fixed

---
+++ Learning/React/firebaseAuthentifications/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJYxXuueMuq3TfcnC0EvNwwxAsOuCahFI",
+  authDomain: "orexoviy-sad.firebaseapp.com",
+  databaseURL: "https://orexoviy-sad-default-rtdb.firebaseio.com",
+  projectId: "orexoviy-sad",
+  storageBucket: "orexoviy-sad.appspot.com",
+  messagingSenderId: "990091375673",
+  appId: "1:990091375673:web:90f90796d4edf72625c9e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
